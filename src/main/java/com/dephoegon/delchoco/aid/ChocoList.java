ae84bd593137f1408cc12624ec965b142ff6361a
Kerns Noel(Acer) <45953503+DePhoegon@users.noreply.github.com>
2023-09-28 07:16:54 -0600

    3.3.6 - Patch Release
    Signed-off-by: Kerns Noel(Acer) <45953503+DePhoegon@users.noreply.github.com>
    
    Took 1 hour 59 minutes

--- src/main/java/com/dephoegon/delchoco/aid/ChocoList.java
+++ src/main/java/com/dephoegon/delchoco/aid/ChocoList.java
@@ -3,12 +3,14 @@ package com.dephoegon.delchoco.aid;
 import com.dephoegon.delchoco.aid.world.serverVariableAccess;
 import com.dephoegon.delchoco.client.gui.RenderChocoboOverlay;
 import com.dephoegon.delchoco.client.keybind.KeyBindManager;
+import com.dephoegon.delchoco.common.commands.chocoboTeams;
 import com.dephoegon.delchoco.common.handler.LootTableEventHandler;
 import com.dephoegon.delchoco.common.init.ModEntities;
 import com.dephoegon.delchoco.common.init.ModItems;
 import com.dephoegon.delchoco.common.init.ModSounds;
 import com.dephoegon.delchoco.common.network.PacketManager;
 import com.dephoegon.delchoco.common.world.worldgen.ModWorldGen;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 
 public class ChocoList {
     public static void clientRegOrder() {
@@ -17,6 +19,7 @@ public class ChocoList {
     }
     public static void commonRegOrder() {
         ModEntities.registerAttributes();
+        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> chocoboTeams.commands(dispatcher));
         ModSounds.registerSounds();
         PacketManager.init();
         serverVariableAccess.init();
